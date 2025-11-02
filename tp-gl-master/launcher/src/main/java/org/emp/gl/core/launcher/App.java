8b606bf60f0cb306334122479cd044a32070f01a
you@example.com <you@example.com>
2025-11-02 14:19:05 +0100

    Étape 4 : Horloge connectée au TimerService

--- tp-gl-master/launcher/src/main/java/org/emp/gl/core/launcher/App.java
+++ tp-gl-master/launcher/src/main/java/org/emp/gl/core/launcher/App.java
@@ -1,24 +1,18 @@
 package org.emp.gl.core.launcher;
 
-import org.emp.gl.clients.Horloge ;
+import org.emp.gl.clients.Horloge;
+import org.emp.gl.time.service.impl.DummyTimeServiceImpl;
 
-/**
- * Hello world!
- *
- */
 public class App {
+    public static void main(String[] args) throws InterruptedException {
+        // إنشاء الخدمة الزمنية
+        DummyTimeServiceImpl timer = new DummyTimeServiceImpl();
 
-    public static void main(String[] args) {
+        // إنشاء ساعة وربطها بالمؤقت
+        Horloge h1 = new Horloge("Num 1");
+        h1.setTimerService(timer);
 
-        testDuTimeService();
-    }
-
-    private static void testDuTimeService() {
-        Horloge horloge = new Horloge("Num 1") ;
-    }
-
-    public static void clearScreen() {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
+        // تشغيل التطبيق لمدة 10 ثوانٍ
+        Thread.sleep(10000);
     }
 }
--- tp-gl-master/timer-service-client/src/main/java/org/emp/gl/clients/Horloge.java
+++ tp-gl-master/timer-service-client/src/main/java/org/emp/gl/clients/Horloge.java
@@ -1,26 +1,33 @@
-package org.emp.gl.clients ; 
+package org.emp.gl.clients;
 
-import org.emp.gl.timer.service.TimerService ; 
+import org.emp.gl.timer.service.TimerService;
+import org.emp.gl.timer.service.TimerChangeListener;
 
+public class Horloge implements TimerChangeListener {
+    String name;
+    TimerService timerService;
 
-public class Horloge {
-
-    String name; 
-    TimerService timerService ; 
-
-
-    public Horloge (String name) {
-        this.name = name ; 
-
-        System.out.println ("Horloge "+name+" initialized!") ;
+    public Horloge(String name) {
+        this.name = name;
+        System.out.println("Horloge " + name + " initialized!");
     }
 
-    public void afficherHeure () {
+    public void setTimerService(TimerService timerService) {
+        this.timerService = timerService;
+        timerService.addTimeChangeListener(this);
+    }
+
+    @Override
+    public void propertyChange(String propertyName, Object oldValue, Object newValue) {
+        // cette méthode est appelée à chaque tick du timer
+        afficherHeure();
+    }
+
+    public void afficherHeure() {
         if (timerService != null)
-            System.out.println (name + " affiche " + 
-                                timerService.getHeures() +":"+
-                                timerService.getMinutes()+":"+
-                                timerService.getSecondes()) ;
+            System.out.println(name + " affiche " +
+                    timerService.getHeures() + ":" +
+                    timerService.getMinutes() + ":" +
+                    timerService.getSecondes());
     }
-
 }
